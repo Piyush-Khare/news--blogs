6d4b9196fb740719c117152cf10954bcca483671
unknown <piyush.khare8@gmail.com>
2021-09-07 19:22:17 +0530

    New changes

--- blogNews/settings.py
+++ blogNews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-m(^=79yu*fnvz7496@bai7v*0+@bsds&m!bou_5t5ffe+!l(+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['news--blogs.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
